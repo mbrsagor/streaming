a0018be7c5fa934659c24ae377f2377097e8c5bd
mbrsagor <mbrsagor@gmail.com>
2020-07-04 18:46:53 +0600

    Admin panel added publication & article

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models.publication import *
+from core.models.article import *
 
-# Register your models here.
+
+admin.site.register(Publication)
+admin.site.register(Article)
