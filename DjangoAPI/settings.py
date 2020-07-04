5ae800fdedee05b83a3f0aa65034be63683d0a23
mbrsagor <mbrsagor@gmail.com>
2020-07-04 21:00:44 +0600

    Token authentication sytem added

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -57,6 +57,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# REST FRAMEWORK SETTINGS
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'drf_role.permissions.BaseRolePermission',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'PAGINATE_BY_PARAM': 'page_size'
+}
+
 ROOT_URLCONF = 'DjangoAPI.urls'
 
 TEMPLATES = [
