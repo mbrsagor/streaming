56de395cb0cf657f164b7585c2e534642ed8522a
Sagor <mbrsagor@gmail.com>
2020-12-25 23:24:39 +0600

    Custom permissions

---
+++ core/helpers/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class PermissionHelperMixin:
+    def admin_editable_only(self):
+        if self.action not in ['list', 'retrieve']:
+            return [permissions.IsAdminUser()]
+        else:
+            return []
+
+    def authenticated_user_editable_only(self):
+        if self.action not in ['list', 'retrieve']:
+            return [permissions.IsAuthenticated()]
+        else:
+            return []
