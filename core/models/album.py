e3b033e0daa648de5dc42b8513f0e24b1e817d52
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-03-31 21:46:36 +0600

    Add new field Album models

--- core/models/album.py
+++ core/models/album.py
@@ -1,8 +1,10 @@
 from core.models.base import BaseEntity
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Album(BaseEntity):
+    user = parent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photo_user')
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="album")
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='photo_set')
