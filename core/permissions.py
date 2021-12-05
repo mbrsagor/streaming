18416c9135ac2266feefeba294026fbdfb98e064
mbrsagor <mbrsagor@gmail.com>
2021-12-05 20:27:37 +0600

    New: add new permission and service valdiation added

--- core/permissions.py
+++ core/permissions.py
@@ -1,6 +1,18 @@
 from rest_framework import permissions
 from datetime import datetime, timedelta, timezone
 
+class PermissionHelperMixin(object):
+    def admin_editable_only(self):
+        if self.action not in ['list', 'retrieve']:
+            return [permissions.IsAdminUser()]
+        else:
+            return []
+
+    def authenticated_user_editable_only(self):
+        if self.action not in ['list', 'retrieve']:
+            return [permissions.IsAuthenticated()]
+        else:
+            return []
 
 class AuthorAllStaffAllButEditOrReadOnly(permissions.BasePermission):
     edit_methods = ("PUT", "PATCH")
