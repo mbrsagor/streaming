a237e28d981cf360f7df10a3e9155082c8f2b2cf
mbrsagor <mbrsagor@gmail.com>
2021-12-09 20:37:31 +0600

    Refactor: Profile API endpoint refactor

--- core/views/category_view.py
+++ core/views/category_view.py
@@ -4,6 +4,7 @@ from core.serializers.category_serializer import CategorySerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
