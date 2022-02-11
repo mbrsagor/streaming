9673c767f4ec946eecfbb0437119e65e17fb2d0e
mbrsagor <mbrsagor@gmail.com>
2022-02-12 00:18:16 +0600

    Refactor: Module  models fields requied false

--- core/models/student.py
+++ core/models/student.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Module(models.Model):
     module_name = models.CharField(max_length=120)
-    class_room = models.CharField(max_length=20)
-    module_desc = models.TextField()
+    class_room = models.CharField(max_length=20, blank=True, null=True)
+    module_desc = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.module_name
