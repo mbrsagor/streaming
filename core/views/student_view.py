75d98acee9c7fdc026a5aefc31cf434cc04fd538
mbrsagor <mbrsagor@gmail.com>
2022-02-20 13:46:02 +0600

    New: Student json body example added

--- core/views/student_view.py
+++ core/views/student_view.py
@@ -13,3 +13,24 @@ class ModuleViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    # Json Body
+    """
+    {
+        "name": "Best practices through leveraged.",
+        "age": 28,
+        "grade": 444,
+        "modules": [
+            {
+                "module_name": "update new modues",
+                "class_room": "new seven",
+                "module_desc": "Conveniently reconceptualize go forward collaboration and idea-sharing through extensive information. Progressively actualize enterprise alignments rather than synergistic technology. Synergistically deliver multifunctional best practices through leveraged best practices."
+            },
+            {
+                "module_name": "new last module",
+                "class_room": "new nine",
+                "module_desc": "Authoritatively transition open-source technology without functional markets. Globally administrate."
+            }
+        ]
+    }
+    """
