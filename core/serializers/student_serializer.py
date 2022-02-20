d105ff07dba170a1b32e7ecd94701249e536b05e
mbrsagor <mbrsagor@gmail.com>
2022-02-20 13:17:27 +0600

    Bug: Student ManyToMany relation bug resolved

--- core/serializers/student_serializer.py
+++ core/serializers/student_serializer.py
@@ -12,9 +12,24 @@ class ModuleSerializer(serializers.ModelSerializer):
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True)
+
+    def get_or_create_module(self, modules):
+        modules_ids = []
+        for module in modules:
+            module_instance, create = Module.objects.get_or_create(pk=module.get('id'), defaults=module)
+            modules_ids.append(module_instance.pk)
+        return modules_ids
+
+    def create(self, validated_data):
+        module = validated_data.pop('modules', [])
+        student = Student.objects.create(**validated_data)
+        student.modules.set(self.get_or_create_module(module))
+        return student
+
     class Meta:
         model = Student
-        depth = 1
+        # depth = 1
         fields = [
             'id', 'name', 'age', 'grade', 'modules'
         ]
--- core/views/student_view.py
+++ core/views/student_view.py
@@ -13,17 +13,3 @@ class ModuleViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        new_student = Student.objects.create(
-            name=data['name'], age=data['age'], grade=data['grade']
-        )
-        new_student.save()
-
-        for module in data['modules']:
-            module_obj = Module.objects.get(module_name=module['module_name'])
-            new_student.modules.add(module_obj)
-
-        serializer = StudentSerializer(new_student)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
