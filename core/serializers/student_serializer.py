4e2423ddc85f5d0a65d3332e415e4be52c75d065
mbrsagor <mbrsagor@gmail.com>
2022-02-20 13:28:12 +0600

    Bug: Student update relation bug resolved

--- core/serializers/student_serializer.py
+++ core/serializers/student_serializer.py
@@ -21,15 +21,33 @@ class StudentSerializer(serializers.ModelSerializer):
             modules_ids.append(module_instance.pk)
         return modules_ids
 
+    def create_or_update_module(self, modules):
+        modules_ids = []
+        for module in modules:
+            module_instance, create = Module.objects.update_or_create(pk=module.get('id'), defaults=module)
+            modules_ids.append(module_instance.pk)
+        return modules_ids
+
     def create(self, validated_data):
         module = validated_data.pop('modules', [])
         student = Student.objects.create(**validated_data)
         student.modules.set(self.get_or_create_module(module))
         return student
 
+    def update(self, instance, validated_data):
+        modules = validated_data.pop('modules', [])
+        instance.modules.set(self.create_or_update_module(modules))
+        fields = ['student_id', 'name', 'age', 'grade']
+        for field in fields:
+            try:
+                setattr(instance, field, validated_data[field])
+            except KeyError:  # validated_data may not contain all fields during HTTP PATCH
+                pass
+        instance.save()
+        return instance
+
     class Meta:
         model = Student
-        # depth = 1
         fields = [
             'id', 'name', 'age', 'grade', 'modules'
         ]
