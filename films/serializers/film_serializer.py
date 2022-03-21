3aea21688798c7e2e4a426c91d64c5fba5113cb3
mbrsagor <mbrsagor@gmail.com>
2022-03-21 23:44:30 +0600

    Bug: films create bug resovle

--- films/serializers/film_serializer.py
+++ films/serializers/film_serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
-from films.models import Film
+from films.models import Film, Actor
+from films.serializers.trailer_serializer import ActorSerializer
 
 
 class FilmSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True)
+
     class Meta:
         model = Film
         read_only_fields = ('director',)
@@ -12,3 +15,16 @@ class FilmSerializer(serializers.ModelSerializer):
             'is_publish', 'release_date', 'description', 'price', 'discount_price',
             'image', 'video', 'is_watchable', 'created_at', 'updated_at'
         ]
+
+    def get_or_create_actor(self, actors):
+        actor_ids = []
+        for actor in actors:
+            actor_instance, create = Actor.objects.get_or_create(pk=actor.get('id'), defaults=actor)
+            actor_ids.append(actor_instance.pk)
+        return actor_ids
+
+    def create(self, validated_data):
+        actors = validated_data.pop('actors', [])
+        film = Film.objects.create(**validated_data)
+        film.actors.set(self.get_or_create_actor(actors))
+        return film
--- films/serializers/trailer_serializer.py
+++ films/serializers/trailer_serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from films.models import Trailer
+from films.models import Trailer, Actor
 
 
 class TrailerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trailer
         fields = '__all__'
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = (
+            'id', 'name'
+        )
