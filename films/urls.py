ea75e43baf7248961cc4c5de0af65e53400fb5b1
mbrsagor <mbrsagor@gmail.com>
2022-03-22 16:40:22 +0600

    New: trailer API update, details, delete done

--- films/urls.py
+++ films/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from films.views.category_view import CategoryCreateListView, CategoryUpdateDeleteAPIView
-from films.views.trailer_view import TrailerCreateListView
+from films.views.trailer_view import TrailerCreateListView, TrailerListView, TrailerUpdateDeleteAPIView, \
+    TrailerDetailsView
 from films.views.film_view import FilmCreateAPIView, FilmListView, DirectorOwnMovieList, FilmDetailsView, \
     FilmUpdateDeleteAPIView
 
@@ -11,6 +12,9 @@ urlpatterns = [
     path('category/<pk>/', CategoryUpdateDeleteAPIView.as_view()),
     # Trailers
     path('trailers/', TrailerCreateListView.as_view()),
+    path('trailer/<pk>/', TrailerUpdateDeleteAPIView.as_view()),
+    path('trailer/detail/<pk>/', TrailerDetailsView.as_view()),
+    path('trailer-list/', TrailerListView.as_view()),
     # Film
     path('create-film/', FilmCreateAPIView.as_view()),
     path('films/', FilmListView.as_view()),
--- films/views/trailer_view.py
+++ films/views/trailer_view.py
@@ -1,14 +1,15 @@
-from rest_framework import generics, status
+from rest_framework import generics, views, status, permissions
 from rest_framework.response import Response
+from django_filters import rest_framework as filters
 
 from films.models import Trailer
 from films.serializers.trailer_serializer import TrailerSerializer
-from utils.response import prepare_create_success_response, prepare_error_response
+from utils.response import prepare_create_success_response, prepare_success_response, prepare_error_response
 from utils.role_util import allow_access_admin, allow_access_manager
 
 
 class TrailerCreateListView(generics.ListCreateAPIView):
-    queryset = Trailer.objects.filter(is_publish=True)
+    queryset = Trailer.objects.all()
     serializer_class = TrailerSerializer
 
     def post(self, request, *args, **kwargs):
@@ -24,3 +25,54 @@ class TrailerCreateListView(generics.ListCreateAPIView):
                 return Response(prepare_error_response(str(e)))
         else:
             return Response(prepare_error_response('You have no permission to create trailer.'))
+
+
+class TrailerUpdateDeleteAPIView(views.APIView):
+
+    def get_object(self, pk):
+        try:
+            return Trailer.objects.get(id=pk)
+        except Trailer.DoesNotExist:
+            return None
+
+    def put(self, request, pk):
+        role = self.request.user.role
+        if role == allow_access_admin or role == allow_access_manager:
+            trailer = self.get_object(pk)
+            if trailer is not None:
+                serializer = TrailerSerializer(trailer, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(prepare_create_success_response(serializer.data), status=status.HTTP_201_CREATED)
+                return Response(prepare_error_response(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(prepare_error_response('No ID found the trailer'), status=status.HTTP_200_OK)
+        else:
+            return Response(prepare_error_response('You have no permission to update the trailer'),
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+    def delete(self, request, pk):
+        role = self.request.user.role
+        if role == allow_access_admin or role == allow_access_manager:
+            trailer = self.get_object(pk)
+            if trailer is not None:
+                trailer.delete()
+                return Response(prepare_success_response('Trailer deleted successfully.'), status=status.HTTP_200_OK)
+            return Response(prepare_error_response('No ID found the trailer'), status=status.HTTP_200_OK)
+        else:
+            return Response(prepare_error_response('You have no permission to delete the trailer'),
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+
+class TrailerDetailsView(generics.RetrieveAPIView):
+    queryset = Trailer.objects.filter(is_publish=True)
+    serializer_class = TrailerSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class TrailerListView(generics.ListAPIView):
+    queryset = Trailer.objects.filter(is_publish=True)
+    serializer_class = TrailerSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('name',)
+    permission_classes = (permissions.AllowAny,)
