ff8d6e44a33659ddbc6073988c518bff5ce5f4ae
mbrsagor <mbrsagor@gmail.com>
2022-03-31 19:16:46 +0600

    New: password reset and rest-complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import views as auth_view
 
 from accounts import views
 
@@ -11,7 +12,9 @@ urlpatterns = [
     path('logout/', views.LogoutAPIView.as_view()),
     path('chanage-password/<pk>/', views.ChangePasswordView.as_view()),
     path('password-reset/', views.PasswordResetView.as_view()),
-    path('password-reset-confirm/', views.PasswordResetConfirmView.as_view()),
+    path('password-change-confirm/', views.PasswordResetConfirmView.as_view()),
+    path('password-reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(), name='password-reset'),
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(), name='password-reset-complete'),
     path('profile/', views.ProfileAPIView.as_view()),
     path('profile/<pk>/', views.ProfileUpdateDeleteView.as_view()),
     path('message/', views.SendMessage.as_view()),
