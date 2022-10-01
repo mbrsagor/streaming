a20abd28f53e75f6bcf18b803c76573d682fa48c
mbrsagor <mbrsagor@gmail.com>
2022-10-01 17:40:56 +0600

    New: add payment add.

--- streaming/urls.py
+++ streaming/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('accounts.urls')),
     path('api/films/', include('films.urls')),
+    path('api/payment/', include('payment.urls')),
 ]
 
 if settings.DEBUG:
