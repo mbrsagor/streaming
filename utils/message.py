370a1c8650077250aa519a25285d7a0eb0ced77f
mbrsagor <mbrsagor@gmail.com>
2023-01-02 22:28:58 +0600

    Refactor: messages refactor.

--- utils/message.py
+++ utils/message.py
@@ -1,5 +1,7 @@
-PERMISSION = 'You no permission.'
-NOID = 'Not found for this ID.'
-DELETED = 'Deleted successfully done.'
-PURCHASE = 'No purchase history found.'
-WARNING = 'something went to wrong'
+from django.utils.translation import gettext as _
+
+PERMISSION = _('You no permission.')
+NOID = _('Not found for this ID.')
+DELETED = _('Deleted successfully done.')
+PURCHASE = _('No purchase history found.')
+WARNING = _('something went to wrong')
